6ef2887ce877012c9d658f412a2b4b74c588b61c
Frank Karlitschek <frank@owncloud.org>
2012-08-26 23:15:19 +0200

    basic movies app. It already lists the movies in ownCloud. Is someone motivated to add a html5 video player that open on click? ;-)
    In Version 2 this app should show automatically generated thumbnails of the movies

---
+++ movies/lib/movies.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * ownCloud - Movies App
+ *
+ * @author Frank Karlitschek
+ * @copyright 2012 Frank Karlitschek frank@owncloud.org
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU AFFERO GENERAL PUBLIC LICENSE
+ * License as published by the Free Software Foundation; either 
+ * version 3 of the License, or any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU AFFERO GENERAL PUBLIC LICENSE for more details.
+ *  
+ * You should have received a copy of the GNU Lesser General Public 
+ * License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
+
+
+namespace OCA_Movies;
+
+class Storage {
+
+	public static function getMovies() {
+		$movies=array();
+		$list=\OC_FileCache::searchByMime('video' );
+		foreach($list as $l) {
+			$info=pathinfo($l);
+			$size=\OC_Filesystem::filesize($l);
+			$mtime=\OC_Filesystem::filemtime($l);
+
+			$entry=array('url'=>$l,'name'=>$info['filename'],'size'=>$size,'mtime'=>$mtime);
+			$movies[]=$entry;
+		}
+
+	
+		return $movies;
+	}
+	
+
+}
+
+?>
---
+++ movies/templates/movies.php
@@ -0,0 +1,26 @@
+<?php
+
+
+if(empty($_['list'])) {
+	
+	    echo('<div id="emptyfolder">No movies are found. Please upload a movie into your ownCloud</div>');		
+
+}else{
+
+	echo('<table class="movieslist" >');
+	foreach($_['list'] as $entry) {
+		echo('
+		<tr>
+			<td width="1"><img align="left" src="'.\OCP\Util::linkToAbsolute('movies','img/movies.png').'"></td>
+			<td><a target="_blank" href="'.\OCP\Util::linkToAbsolute('files','download.php').'&file='.urlencode($entry['url']).'&name='.urlencode($entry['name']).'">'.$entry['name'].'</a></td>
+			<td>'.\OCP\Util::formatDate($entry['mtime']).'</td><td>'.\OCP\Util::humanFileSize($entry['size']).'</td>
+		</tr>'
+		);
+	}
+	echo('</table>');
+	
+	
+	
+}
+
+
